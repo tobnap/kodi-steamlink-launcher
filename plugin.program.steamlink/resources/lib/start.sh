c78a31d8af4fec6fa5dbb37222203eb8d52686fa
BuildTools <unconfigured@null.spigotmc.org>
2020-03-24 20:43:18 -0400

    changed from kodi user to pi

--- plugin.program.steamlink/resources/lib/start.sh
+++ plugin.program.steamlink/resources/lib/start.sh
@@ -2,7 +2,7 @@
 
 DIRNAME=$( dirname "${BASH_SOURCE[0]}" )
 
-sudo openvt -c 7 -s -f -- su kodi -c "bash ${DIRNAME}/heartbeat.sh & /usr/bin/steamlink &"
+sudo openvt -c 7 -s -f -- su pi -c "bash ${DIRNAME}/heartbeat.sh & /usr/bin/steamlink &"
 sudo systemctl stop kodi
 
 exit
